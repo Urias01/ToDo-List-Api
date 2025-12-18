37d72a04ca02e25a6528405eacaa198382378fc0
Urias01 <uriasluis64@gmail.com>
2025-12-18 18:51:16 -0300

    chore: added global exception handler

---
+++ src/main/java/com/todo/exceptions/GlobalException.java
@@ -0,0 +1,33 @@
+package com.todo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.todo.models.responses.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalException {
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ApiResponse<Void>> handleNotFoundException(NotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.failure(ex.getMessage()));
+  }
+
+  @ExceptionHandler(AlreadyExistsException.class)
+  public ResponseEntity<ApiResponse<Void>> handleAlreadyExistsException(AlreadyExistsException ex) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResponse.failure(ex.getMessage()));
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<ApiResponse<Void>> handleBadRequestException(BadRequestException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.failure(ex.getMessage()));
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<ApiResponse<Void>> handleAuthenticationException(AuthenticationException ex) {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.failure(ex.getMessage()));
+  }
+}
+
